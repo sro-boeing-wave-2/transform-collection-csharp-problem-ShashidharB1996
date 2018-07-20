0c7f4730ae906b7048930f5b046d66ce402f7bd5
Shashhidhar B <shashishashidhar70@gmail.com>
2018-07-20 14:35:36 +0530

    transform using delegates assignment donegit add .

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -6,9 +6,17 @@ namespace Transform
 {
     public static class Transform 
     {
-        public static void Map()
+        public delegate int map(int value);
+        public static List<int> Map(this int[] value, map item)
         {
-            throw new NotImplementedException();
+            List<int> mapList = new List<int>();
+            foreach(int x in value)
+            {
+                mapList.Add(item(x));
+            }
+
+            return mapList;
+            //throw new NotImplementedException();
         }
     }
 }
